5c8e75d89be940594521fdaf7e6628b1c2af74cb
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-01 18:48:46 -0600

    Setup the initial migration to create the businness db

--- data/migrations/20200501174020_initial.js
+++ data/migrations/20200501174020_initial.js
@@ -1,7 +1,58 @@
-exports.up = function(knex) {
-  
-};
+exports.up = async function(knex) {
 
-exports.down = function(knex) {
-  
-};
+    /////////////// projects ///////////////
+    
+        await knex.schema.createTable("projects", (table) => {
+            table.increments("id")
+            table.text("name").notNullable()
+            table.text("description").nullable()
+            table.boolean("completed").defaultTo(false)
+        })
+    
+    /////////////// tasks ///////////////
+    
+        await knex.schema.createTable("tasks", (table) => {
+            table.increments("id")
+            table.text("name").notNullable()
+            table.text("description").notNullable()
+            table.text("notes").nullable()
+            table.boolean("completed").defaultTo(false)
+        })
+    
+    /////////////// resources ///////////////
+    
+        await knex.schema.createTable("resources", (table) => {
+            table.increments("id")
+            table.text("name").notNullable()
+            table.text("description").nullable()
+
+        })
+    
+    /////////////// projects_resources ///////////////
+    
+        await knex.schema.createTable("projects_resources", (table) => {
+            table.integer("project_id")
+                .references("id")
+                .inTable("projects")
+                .onDelete("CASCADE")
+                .onUpdate("CASCADE")
+    
+            table.integer("resource_id")
+                .references("id")
+                .inTable("resources")
+                .onDelete("CASCADE")
+    
+            table.primary(["project_id", "resource_id"])
+        })
+    
+    
+    };
+    
+    exports.down = async function(knex) {
+        await knex.schema.dropTableIfExists("projects_resources")
+        await knex.schema.dropTableIfExists("resources")
+        await knex.schema.dropTableIfExists("tasks")
+        await knex.schema.dropTableIfExists("projects")
+    
+    };
+    
