9176570b1a0f7a34380d3b0209861369ec930af2
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-04 18:54:30 -0600

    Built seed data for projects_resources.

--- data/seeds/02_resources.js
+++ data/seeds/02_resources.js
@@ -4,5 +4,10 @@ exports.seed = async function(knex) {
     { name: "Bucket", description: "" },
     { name: "Wet Rag", description: "It is a rag that is wet." },
     { name: "Vaccum", description: "It really sucks." },
+    { name: "Garbage Bags", description: "Bags for garbage." },
+    { name: "Shovel", description: "It's for diggin." },
+    { name: "Dirt", description: "It's for planting." },
+    { name: "Watering Can", description: "To water things with." },
+
 	])
 }
